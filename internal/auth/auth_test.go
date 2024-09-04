bdc354f35bf5f529f04d0cc7006add608fa0a5a3
Sedat <sedat.capar@isbank.net.tr>
2024-09-04 19:48:00 +0300

    Auto format

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,18 +6,18 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
- 
-    headers := http.Header{}
-    headers.Set("Authorization", "ApiKey expected-key-value")
 
-    apiKey, err := GetAPIKey(headers)
- 
-    if err != nil {
-        t.Fatalf("expected no error, got %v", err)
-    }
-    expectedAPIKey := "expected-key-value"
-    if apiKey != expectedAPIKey {
-        t.Errorf("expected %v, got %v", expectedAPIKey, apiKey)
-    }
- 
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey expected-key-value")
+
+	apiKey, err := GetAPIKey(headers)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	expectedAPIKey := "expected-key-value"
+	if apiKey != expectedAPIKey {
+		t.Errorf("expected %v, got %v", expectedAPIKey, apiKey)
+	}
+
 }
